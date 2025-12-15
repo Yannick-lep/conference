60179ee796aa61118b0775eaae3f683649ba7e3d
Yannick-lep <lepinayyannick7@gmail.com>
2025-12-15 16:51:31 +0100

    creation edit-conf

--- voir-conf.php
+++ voir-conf.php
@@ -2,9 +2,16 @@
 include 'fonctions.php';
 require 'connexion-bd.php';
 
-$idSupp = $_GET['id'] ?? null;
+$idVoir = $_GET['id'] ?? null;
 if ( ! is_numeric($idSupp)) {
     dd("Cette conference n'existe pas !!!");
 }
+$pdo = new PDO($dsn, $user, $pass, $options);
+$stm = $pdo->prepare("SELECT * FROM participants where id = :id");
+$stm->bindParam(':id', $idVoir, PDO::PARAM_INT);
+$stmConf = $stm->execute();
+$conference = $stm->fetch();
+
+dd($conference);
 
 ?>
